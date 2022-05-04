7de345877d8fcc4104dc1d8fad13c24131b8d5cd
nolen <lenonalcant@hotmail.com>
2022-05-03 23:24:35 -0300

    dom

--- javascript/02-dom/0308-traversing-e-manipulacao/scriptExercicio.js
+++ javascript/02-dom/0308-traversing-e-manipulacao/scriptExercicio.js
@@ -1,7 +1,20 @@
 // Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
+const cloneMenu = menu.cloneNode(true);
+
+copy.appendChild(cloneMenu);
 
 // Selecione o primeiro DT da dl de Faq
+const firstDT = document.querySelector('.faq-lista');
+
+console.log(firstDT.children[0]);
 
 // Selecione o DD referente ao primeiro DT
+console.log(firstDT.nextElementSibling);
 
 // Substitua o conteúdo html de .faq pelo de .animais
+const animais = document.querySelector('.animais');
+const faq = document.querySelector('.faq');
+
+faq.innerHTML = animais.innerHTML;
