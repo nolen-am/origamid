1ad82bfcf079595ad8e8ca05168ff9e2143d33f9
nolen <lenonalcant@hotmail.com>
2022-05-04 21:12:49 -0300

    dom

--- javascript/02-dom/0311-scroll-suave-link-interno/script.js
+++ javascript/02-dom/0311-scroll-suave-link-interno/script.js
@@ -40,3 +40,33 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+    
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    
+        // forma alternativa:
+        // const topo = section.offsetTop;
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior: 'smooth',
+        // });
+    
+        console.log(topo)
+    }
+    
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
